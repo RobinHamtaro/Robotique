207cd27197a46b1e2bac46045bdfe9475e53cba6
RobinHamtaro <robin.beilvert@gmail.com>
2013-10-19 16:33:56 +0200

    Update robotarm.ino

--- robotarm/robotarm.ino
+++ robotarm/robotarm.ino
@@ -3,93 +3,76 @@
 
 
 
-
-
-int button11=0; // axe z1
+int button11=0;
 int button12=0;
-
-int button21=0; // axe y1
+int button21=0;
 int button22=0;
-
-int button31=0;// axe y2
-int button32=0;
-
-int button41=0;// axe z2
-int button42=0;
-
-int button51=0;// axe y3
-int button52=0;
-
-int capteur1,capteur2,capteur3,capteur4,capteur5; // 5 capteurs
 
 void setup ()
 {
-  pinMode (2,OUTPUT);  // 
-  pinMode (3, OUTPUT); // 
- 
- 
   pinMode (4,INPUT);
-  pinMode (5, INPUT);
- 
- 
-  pinMode (6,OUTPUT);
-  pinMode (7, OUTPUT);
- 
- 
-  pinMode (4,INPUT);
-  pinMode (5, INPUT);
- 
- 
+  pinMode (5,INPUT); 
+  pinMode (6,INPUT);
+  pinMode (7,INPUT);
+  
+  pinMode (0,OUTPUT);
+  pinMode (1,OUTPUT);
+  pinMode (2,OUTPUT);
+  pinMode (3,OUTPUT);
+
+  
 }
 
 void loop ()
 {
+  button11=digitalRead (4);
+  button12=digitalRead (5);
+  button21=digitalRead (6);
+  button22=digitalRead (7);
   
+  ///////////////////Moteur 1///////////////////////
+  if (button11==1 && button12 ==0)           
+  {
+   digitalWrite (0,1);
+   digitalWrite (1,1);
 
-  button1=digitalRead (4);
-  button2=digitalRead (5);
+  }
+    else if (button12==1 && button11 ==0)
+  {
+   digitalWrite (0,1);
+
+  }
+    else
+  {
+   digitalWrite (0,0);
+   digitalWrite (1,0);
+  }
+  //////////////////////////////////////////////////
   
-  
-  
-  
-  
-  ////////////////////////////////initialisation des capteurs ////////////////////
- capteur0= analogRead(0);
- capteur1= analogRead(1);  
- capteur2= analogRead(2);  
- capteur3= analogRead(3);
- capteur4= analogRead(4);
-  
-  
- ///////////////////////////////////Début de la sequence ////////////////////// 
-  
-  
-  
- 
-  if (button1==1 && button2 ==0) //fait tourner à gauche sur l'axe z1
+  ///////////////////Moteur 2///////////////////////
+   if (button21==1 && button22 ==0)
   {
    digitalWrite (2,1);
    digitalWrite (3,1);
 
   }
-    else if (button2==1 && button1 ==0) //fait tourner à droite sur l'axe z1
+    else if (button22==1 && button21 ==0)
   {
    digitalWrite (2,1);
 
   }
-    else // il ne se passe rien
+    else
   {
    digitalWrite (2,0);
    digitalWrite (3,0);
-
   }
+  //////////////////////////////////////////////////
 delay(30);
- 
- 
-   
- 
+  
+  
+    
+  
 
- 
- 
+  
+  
 }
-
